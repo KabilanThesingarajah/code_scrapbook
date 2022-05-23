db921c928a9f32f92ef5031f8a258fbd5a3afd13
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-05-23 20:22:26 +0100

    I learned about input using scanf
    I think I need to learn how to format printing statements next.

---
+++ c/input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    
+    int my_input;
+    printf("enter a number... ");
+    scanf("%d", &my_input);
+    printf("the number was : %d", my_input);
+    return 0;
+
+}
