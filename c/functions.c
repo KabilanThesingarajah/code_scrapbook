ba0b6336bf6bde39f2452fda80c1b4fa9f573123
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-05-26 14:52:50 +0100

    Added functions (prototypes) and loops
    I am slowly starting to appreciate c. It is a language that does have a
    lot of syntax to learn compared to python, but that makes it a lot
    easier to do things beyond the very basics. Everything that is done
    makes a lot of sense.
    
    I learnt functions and loops.

---
+++ c/functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int mutl(int a, int b);
+
+
+int main(){
+    printf("%d",mult(10,20));
+}
+
+
+int mult(int a, int b){
+    return a * b;
+}
+
---
+++ c/loops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    for (int i; i <10; i++){
+        printf("%d\n", i);
+    }
+
+    int x =0;
+    while(x < 10){
+        printf("hello");
+        x++;
+    }
+
+    // continue will go to the next iteration
+    // break will go exit all the iterations
+
+}
