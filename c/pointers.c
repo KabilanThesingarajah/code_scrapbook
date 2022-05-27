1a1c228e428b84efc7b60543dfefb3a90206c1d6
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-05-27 16:45:03 +0100

    feat: pointers

---
+++ c/pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+
+   A pointer is a variable with the syntax *name (see below).
+   When you want to store the address of a variable, you use & (ampersand for address)
+
+ */
+
+int main(){
+    int x = 10; // a normal variable declaration
+    int *pointer = &x; // this pointer points to the address of x
+    float *ptr2;
+    printf("%d",pointer);
+    ptr2 = malloc( sizeof( *ptr)); // used to allocate memory at runtime.
+    free(*ptr2) // free is used only if malloc or similar functions are called, and not when normally pointed as above;
+    return 0;
+}
