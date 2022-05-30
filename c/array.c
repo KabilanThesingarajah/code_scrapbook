5e2bbc25e49b71fd41b255f1609904f32b47689c
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-05-30 18:01:58 +0100

    feat: arrays

---
+++ c/array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Arrays actually store each item in memory adjacently. So we have a special way to use pointers and addresses.
+ */
+
+int main(){
+
+    int examplearray[10];
+    char mystring[10]; // we don't have strings without this.
+
+    return 0;
+}
