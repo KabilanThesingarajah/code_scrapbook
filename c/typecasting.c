0431d6770d80a6b707cec27f557b071ae4e78806
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-06-10 07:20:53 +0100

    feat: typecasting

---
+++ c/typecasting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    /* typecasting is changing the datatype of a variable for a specific use case */
+
+
+    int a = (float) 0.5;
+    printf("%i",a);
+    getchar();
+
+    return 0;
+}
