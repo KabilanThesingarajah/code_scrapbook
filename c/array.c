f626f7138c53f1bffae56b34d40b474c77301b69
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-08-04 16:40:04 +0100

    chore: commented array and other trivial changes

--- c/array.c
+++ c/array.c
@@ -4,10 +4,19 @@
  * Arrays actually store each item in memory adjacently. So we have a special way to use pointers and addresses.
  */
 
+void print_array(int array[],int len);
+
 int main(){
-
-    int examplearray[10];
+    int examplearray[10] = {0}; // if you do not give them an initial value, then they will just have the random chunk of data in that memory before.
+    examplearray[0] = 5; // assignment
     char mystring[10]; // we don't have strings without this.
-
+    print_array(examplearray,10);
     return 0;
+}
+
+void print_array(int array[],int len){
+    for (int i = 0;i < len ; i++){
+        printf("%d , ",array[i]);
+    }
+    printf("\n");
 }
--- c/c_string.c
+++ c/c_string.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    char string[256];
+    char string[256]; // this is a string of 256 characters
     printf("Please enter a string:");
     fgets(string, 256, stdin); // this is a more secure way to get input then scanf
     printf("the string is %s",string);
