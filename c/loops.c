3ca83ed439d9274010b48875b03a38ca70196cd1
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-09-18 19:57:56 +0100

    chore: added comments to loops.c

--- c/loops.c
+++ c/loops.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main(){
-
+    // this is a for loop
     for (int i; i <10; i++){
         printf("%d\n", i);
     }
 
     int x =0;
+    // this is a while loop
     while(x < 10){
         printf("hello");
         x++;
