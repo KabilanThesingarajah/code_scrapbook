641343d572cd9b476d7f80eaa80f423d45efb3d6
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2022-09-19 16:59:17 +0100

    feat: added macros in c

---
+++ c/define.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// defining macros are shown here, and have many powerful application such as in debugging for memory leaks
+#define PRINT_HELLO_WORLD printf("Hello World\n");
+
+int main(){
+
+    PRINT_HELLO_WORLD
+
+    return 0;
+}
