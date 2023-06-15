c46d2fdcbfc9b1476f8f3a58bd4eb90ad67df159
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2023-06-15 10:50:08 +0100

    fix: changed api key and set the key as an env var

--- web_development/theComplete2023WebDevelopmentBootcamp/weatherProject/app.js
+++ web_development/theComplete2023WebDevelopmentBootcamp/weatherProject/app.js
@@ -1,10 +1,11 @@
-
 const express = require("express");
 const app = express();
 const https = require("node:https");
 
+
 app.get("/", (req, res) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=af56282fcc8fa8786e522ae2b5fc60a3&units=metric" ;
+    // This requires an api key setup 
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + process.env.OPENWEATHERMAPKEY + "&units=metric" ;
     https.get(url, (res) => {
         console.log(res);
     });
@@ -14,3 +15,4 @@ app.get("/", (req, res) => {
 app.listen(3000, ()=>{
     console.log("app is listening on port 3000");
 });
+
