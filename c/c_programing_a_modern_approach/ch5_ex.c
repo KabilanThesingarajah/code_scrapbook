739f1cdb725a121365e9938e97a4e6db9bd6b065
Kabilan Thesingarajah <kabilanthesingarajah@gmail.com>
2024-06-06 10:38:02 +0100

    test + fix chapter 5 exercises

--- c/c_programing_a_modern_approach/ch5_ex.c
+++ c/c_programing_a_modern_approach/ch5_ex.c
@@ -3,6 +3,7 @@
 /* Author: Kabilan Thesingarajah */
 
 #include <stdio.h>
+#define AR_LEN 9
 
 void q5_01();
 void q5_02();
@@ -17,6 +18,7 @@ void q5_10();
 void q5_11();
 
 int i, j, k;
+
 
 void
 q5_01()
@@ -33,7 +35,7 @@ q5_01()
 	i = 3; j = 2; k = 1;
 	/* relational beats equality in precedence*/
 	/* both relations are false, so the equality is true */
-	printf("c) %d == 0?\n", i < j == j < k);
+	printf("c) %d == 1?\n", i < j == j < k); // mistake silly
 
 	i = 3; j = 4; k = 5;
 	/* arithmetic beats relational */
@@ -46,6 +48,7 @@ q5_01()
 void
 q5_02()
 {
+	/* correct! */
 	i = 10; j = 5;
 	/* ! beats relational, !i == 0 */
 	printf("a) %d == 1?\n", !i < j);
@@ -65,7 +68,8 @@ q5_02()
 void
 q5_03()
 {
-	i = 3; j = 4; k = 5
+	/* correct! */
+	i = 3; j = 4; k = 5;
 	/* short-circuit meaning rhs is not evaluated */
 	printf("a) %d == 1?\n", i < j || ++j < k);
 	printf("%d %d %d == 3 4 5?\n", i, j, k);
@@ -87,24 +91,43 @@ q5_03()
 
 }
 
+int
+q5_04_helper(int i, int j)
+{
+	return (i > j) - (i < j);
+}
+
 void
 q5_04()
 {
-	(i > j) - (i < j);
+	/* correct */
+	for (i = 0; i < 3; i++){
+		for (j = 0; j < 3; j++){
+			printf("i == %d\n", i);
+			printf("j == %d\n", j);
+			printf("statement == %d\n", q5_04_helper(i, j));
+			printf("\n");
+		}
+	}
 }
 
 void
 q5_05()
 {
+	int n = 0;
 	if (n >= 1 <= 10) printf("n is between 1 and 10\n");
 	/* is legal but not intended, the if is equivalent to */
 	if ((n >= 1) <= 10) printf("n is greater than or equal to 1\n");
+	/* mistake silly */
+	if ((n >= 1) <= 10) printf("n is any int\n");
  
 }
 
 void
 q5_06()
 {
+	/* correct */
+	int n = -9;
 	if (n == 1 - 10) printf("n is between 1 and 10\n");
 	/* is legal but not intended, the if is equivalent to */
 	if (n == 1 - 10) printf("n is equal to -9\n");
@@ -113,74 +136,97 @@ q5_06()
 void
 q5_07()
 {
+	/* correct */
 	/* if i >= 0 do i else do -i */
 	i = 17;
-	printf("%d\n == 17", i >= 0? i : -i);
+	printf("%d == 17\n", i >= 0? i : -i);
 	i = -17;
-	printf("%d\n == 17", i >= 0? i : -i);
+	printf("%d == 17\n", i >= 0? i : -i);
 }
 
 void
 q5_08()
 {
-	int teenager;
+	int teenager, age, true, false;
+	true = 1;
+	false = 0;
+	age = 20;
 	if (13 <= age && age <= 19 )
 			teenager = true;
 		else
 			teenager = false;
+	printf("teenager is %d\n", teenager);
 	
 	/* furthermore */
-	(13 <= age && age <= 19 )? teenager = true : teenager = false
+	teenager = (13 <= age && age <= 19 )? true : false;
+	printf("teenager is %d\n", teenager);
+
+	/* even simpler */
+	teenager = (13 <= age && age <= 19 );
+	printf("teenager is %d\n", teenager);
 
 }
 
+int score = 0;
+
 void
-q5_09()
+q5_09(int *score_ptr)
 {
 	/* The two if statements are equivalent */
-	int score = 0;
-	if (score >= 90) {
+	if (*score_ptr >= 90) {
 		printf("A");
-	} else if (score >= 80) {
+	} else if (*score_ptr >= 80) {
 		printf("B");
-	} else if (score >= 70) {
+	} else if (*score_ptr >= 70) {
 		printf("C");
-	} else if (score >= 60) {
+	} else if (*score_ptr >= 60) {
 		printf("D");
 	} else {
 		printf("F");
+	}
 
 	
-	if (score < 60) {
+	if (*score_ptr < 60) {
 		printf("F");
-	} else if (score < 70) {
+	} else if (*score_ptr < 70) {
 		printf("D");
-	} else if (score < 80) {
+	} else if (*score_ptr < 80) {
 		printf("C");
-	else if (score < 90) {
+	} else if (*score_ptr < 90) {
 		printf("B");
 	} else {
 		printf("A");
+	}
+}
+
+void
+q5_09_tester()
+{
+	for (score = 0; score < 100; score += 5){
+		q5_09(&score);
+		printf("\n");
+	}
 }
 
 void
 q5_10()
 {
+	/* correct */
 	i = 1;
 	switch (i % 3) {
-		case 0: printf("zero");
-		case 1: printf("one");
-		case 2: printf("two");
+		case 0: printf("zero"); /*@fallthrough@*/
+		case 1: printf("one"); /*@fallthrough@*/
+		case 2: printf("two"); /*@fallthrough@*/
 	}
 	/* output one two, as a switch is a jump to label, and fall-through happens */
 	
 }
 
+int area_code = 0;
 void
-q5_11()
+q5_11(int *area_code_ptr)
 {
-	int area_code = 0;
-	switch (area_code) {
+	switch (*area_code_ptr) {
 		case 229:
 			printf("Albany\n");
 			break;
@@ -199,12 +245,23 @@ q5_11()
 	}
 }
 
+void
+q5_11_tester()
+{
+	int area_codes[AR_LEN] = {229, 404, 470, 478, 678, 706, 762, 770, 912};
+	for (int i = 0; i < AR_LEN; i++){
+		printf("area code: %d ", area_codes[i]);
+		q5_11(&(area_codes[i]));
+	}
+
+}
+
 
 int
 main(void)
 {
 
-	
+	q5_11_tester();
 
 	return 0;
 }
